031d1f2e59d22b1839eeb8536cea56255850f87b
Jean-Pierre Delouche <jdelouche@gmail.com>
2020-02-09 13:11:18 +0100

    read

--- catananum.hs
+++ catananum.hs
@@ -12,24 +12,22 @@ ana  :: Functor f => (a -> f a) -> a -> Fix f
 ana coalg = Fix . fmap (ana coalg) . coalg
 data StreamF  e a = StreamF e a deriving (Functor,Show)
 data ChannelF e a = NilF | ChannelF e a deriving (Functor,Show)
-output  ::            Fix (ConnectorF) -> Carrier
-input   :: Carrier -> Fix (ConnectorF)
-output  = cata send
-input   = ana receive
-send   (ChannelF Nothing   (Right p))  = (Right p)
-send   (ChannelF (Just i)  (Right p))  = (Right (i:p))
-send   (NilF)                         = (Right [])
-send::               InterfaceF -> Carrier
-receive:: Carrier -> InterfaceF
-receive (Left [])           = NilF
-receive (Left ('e':'1':ns)) = ChannelF (Just 48) (Left ns)
-receive (Left ('e':'2':ns)) = ChannelF (Just 49) (Left ns)
-receive (Left (p : ns))     = ChannelF (Nothing) (Left ns)
-type Receiver   = [Int]
-type Sender     = [Char]
 type Carrier    = Either Sender Receiver
 type Transfer   = Maybe Int
 type ConnectorF = ChannelF Transfer
 type InterfaceF = ConnectorF Carrier
 read = (output . input)
+output  ::            Fix (ConnectorF) -> Carrier
+input   :: Carrier -> Fix (ConnectorF)
+output  = cata send
+input   = ana receive
+send::               InterfaceF -> Carrier
+receive:: Carrier -> InterfaceF
+send   (ChannelF Nothing   (Right p))  = (Right p)
+send   (ChannelF (Just i)  (Right p))  = (Right (i:p))
+send   (NilF)                         = (Right [])
+receive (Left [])           = NilF
+receive (Left ('e':'1':ns)) = ChannelF (Just 48) (Left ns)
+receive (Left ('e':'2':ns)) = ChannelF (Just 49) (Left ns)
+receive (Left (p : ns))     = ChannelF (Nothing) (Left ns)
 main = do print $ HyloM.read (Left "h1 e2")
