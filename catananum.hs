0691a30d1208cdc9ea209ab5960cc2032c34a75f
Jean-Pierre Delouche <jdelouche@gmail.com>
2020-02-09 11:00:59 +0100

    output . input

--- catananum.hs
+++ catananum.hs
@@ -10,21 +10,21 @@ cata alg  = alg . fmap (cata alg) . unFix;
 ana  :: Functor f => (a -> f a) -> a -> Fix f
 ana coalg = Fix . fmap (ana coalg) . coalg
 data StreamF e a = NilF | StreamF e a deriving (Functor,Show)
-deconstruction ::            Fix (ConnectorF) -> Carrier
-construction   :: Carrier -> Fix (ConnectorF)
-deconstruction = cata send
-construction   = ana receive
-send (StreamF Nothing   (_,Just p))  = (Nothing,Just p)
-send (StreamF (Just i)  (_,Just p))  = (Nothing,Just (i:p))
-send (NilF)                          = (Nothing,Just [])
-send ::               InterfaceF -> Carrier
-receive :: Carrier -> InterfaceF
-receive (Just [],_)           = NilF
-receive (Just ('e':'1':ns),_) = StreamF (Just 48) (Just ns,Nothing)
-receive (Just ('e':'2':ns),_) = StreamF (Just 49) (Just ns,Nothing)
-receive (Just (p : ns),_)     = StreamF (Nothing) (Just ns,Nothing)
-type Carrier    = (Maybe [Char],Maybe [Int])
+ouput   ::            Fix (ConnectorF) -> Carrier
+input   :: Carrier -> Fix (ConnectorF)
+ouput   = cata send
+input   = ana receive
+send   (StreamF Nothing   (Right p))  = (Right p)
+send   (StreamF (Just i)  (Right p))  = (Right (i:p))
+send   (NilF)                         = (Right [])
+send::               InterfaceF -> Carrier
+receive:: Carrier -> InterfaceF
+receive (Left [])           = NilF
+receive (Left ('e':'1':ns)) = StreamF (Just 48) (Left ns)
+receive (Left ('e':'2':ns)) = StreamF (Just 49) (Left ns)
+receive (Left (p : ns))     = StreamF (Nothing) (Left ns)
+type Carrier    = Either [Char] [Int]
 type Transfer   = Maybe Int
 type ConnectorF = StreamF Transfer
 type InterfaceF = ConnectorF Carrier
-main = do print $ (deconstruction . construction) (Just "e1 e2",Nothing)
+main = do print $ (ouput . input) (Left "h1 e2")
