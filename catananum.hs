7523fba2d9c631da8d94dc93334dec5bc6c0740b
Jean-Pierre Delouche <jdelouche@gmail.com>
2020-02-09 13:00:32 +0100

    ChannelF

--- catananum.hs
+++ catananum.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveFunctor #-}
+module HyloM where
 import Prelude
 import Data.Typeable
 data Fix f = Fix (f (Fix f))
@@ -9,25 +10,26 @@ cata :: Functor f => (f a -> a) -> Fix f -> a
 cata alg  = alg . fmap (cata alg) . unFix;
 ana  :: Functor f => (a -> f a) -> a -> Fix f
 ana coalg = Fix . fmap (ana coalg) . coalg
-data StreamF e a = NilF | StreamF e a deriving (Functor,Show)
+data StreamF  e a = StreamF e a deriving (Functor,Show)
+data ChannelF e a = NilF | ChannelF e a deriving (Functor,Show)
 output  ::            Fix (ConnectorF) -> Carrier
 input   :: Carrier -> Fix (ConnectorF)
 output  = cata send
 input   = ana receive
-send   (StreamF Nothing   (Right p))  = (Right p)
-send   (StreamF (Just i)  (Right p))  = (Right (i:p))
+send   (ChannelF Nothing   (Right p))  = (Right p)
+send   (ChannelF (Just i)  (Right p))  = (Right (i:p))
 send   (NilF)                         = (Right [])
 send::               InterfaceF -> Carrier
 receive:: Carrier -> InterfaceF
 receive (Left [])           = NilF
-receive (Left ('e':'1':ns)) = StreamF (Just 48) (Left ns)
-receive (Left ('e':'2':ns)) = StreamF (Just 49) (Left ns)
-receive (Left (p : ns))     = StreamF (Nothing) (Left ns)
+receive (Left ('e':'1':ns)) = ChannelF (Just 48) (Left ns)
+receive (Left ('e':'2':ns)) = ChannelF (Just 49) (Left ns)
+receive (Left (p : ns))     = ChannelF (Nothing) (Left ns)
 type Receiver   = [Int]
 type Sender     = [Char]
 type Carrier    = Either Sender Receiver
 type Transfer   = Maybe Int
-type ConnectorF = StreamF Transfer
+type ConnectorF = ChannelF Transfer
 type InterfaceF = ConnectorF Carrier
 read = (output . input)
-main = do print $ read (Left "h1 e2")
+main = do print $ HyloM.read (Left "h1 e2")
