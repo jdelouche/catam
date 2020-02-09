7ffea729dd84d264225cf7c2ff678cd74810670b
Jean-Pierre Delouche <jdelouche@gmail.com>
2020-02-09 10:46:23 +0100

    Nothing

--- catananum.hs
+++ catananum.hs
@@ -14,17 +14,17 @@ deconstruction ::            Fix (ConnectorF) -> Carrier
 construction   :: Carrier -> Fix (ConnectorF)
 deconstruction = cata send
 construction   = ana receive
-send (StreamF Nothing   (x,p)) = ([],p)
-send (StreamF (Just i)  (x,p)) = ([],i:p)
-send (NilF)                    = ([],[])
+send (StreamF Nothing   (_,Just p))  = (Nothing,Just p)
+send (StreamF (Just i)  (_,Just p))  = (Nothing,Just (i:p))
+send (NilF)                          = (Nothing,Just [])
 send ::               InterfaceF -> Carrier
 receive :: Carrier -> InterfaceF
-receive ([],_)  = NilF
-receive (('e':'1':ns),a) = StreamF (Just 48) (ns,[])
-receive (('e':'2':ns),a) = StreamF (Just 49) (ns,[])
-receive ((p : ns),a)     = StreamF (Nothing) (ns,[])
-type Carrier    = ([Char],[Int])
+receive (Just [],_)           = NilF
+receive (Just ('e':'1':ns),_) = StreamF (Just 48) (Just ns,Nothing)
+receive (Just ('e':'2':ns),_) = StreamF (Just 49) (Just ns,Nothing)
+receive (Just (p : ns),_)     = StreamF (Nothing) (Just ns,Nothing)
+type Carrier    = (Maybe [Char],Maybe [Int])
 type Transfer   = Maybe Int
 type ConnectorF = StreamF Transfer
 type InterfaceF = ConnectorF Carrier
-main = do print $ (deconstruction . construction) ("e1 e2",[])
+main = do print $ (deconstruction . construction) (Just "e1 e2",Nothing)
