02ec730c38e468700fb23a901ca82546c475077c
Jean-Pierre DELOUCHE <jean_pierre.delouche@asn.com>
2020-03-12 17:03:20 +0100

    with ::

--- Fix.hs
+++ Fix.hs
@@ -18,24 +18,24 @@ alg NilF = []
 alg (StreamF e a)  = e:a
 main :: IO ()
 main      = do let a = [2..10]
-               putStr          "                                                                    a : "
+               putStr          "                                                                    a :: "
                print                                                                                a
-               putStr          "                                                                coalg : "
+               putStr          "                                                                coalg :: "
                print $ typeOf $                                                                 coalg
-               putStr          "                                           (fmap (ana coalg)) . coalg : "
+               putStr          "                                           (fmap (ana coalg)) . coalg :: "
                print $ typeOf $                                            (fmap (ana coalg)) . coalg
-               putStr          "                                      Fx . (fmap (ana coalg)) . coalg : "
+               putStr          "                                      Fx . (fmap (ana coalg)) . coalg :: "
                print $ typeOf $                                       Fx . (fmap (ana coalg)) . coalg
-               putStr          "                              unFix . Fx . (fmap (ana coalg)) . coalg : "
+               putStr          "                              unFix . Fx . (fmap (ana coalg)) . coalg :: "
                print $ typeOf $                               unFix . Fx . (fmap (ana coalg)) . coalg
-               putStr          "          (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg : "
+               putStr          "          (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg :: "
                print $ typeOf $           (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg
-               putStr          "    alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg : "
+               putStr          "    alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg :: "
                print $ typeOf $     alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg
-               putStr          "                                             (cata alg) . (ana coalg) : "
+               putStr          "                                             (cata alg) . (ana coalg) :: "
                print $ typeOf $                                              (cata alg) . (ana coalg)
-               putStr          "                                         ((cata alg) . (ana coalg)) a : "
+               putStr          "                                         ((cata alg) . (ana coalg)) a :: "
                print $                                                   ((cata alg) . (ana coalg)) a
-               putStr          "(alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg) a : "
+               putStr          "(alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg) a :: "
                print $          (alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg) a
 
